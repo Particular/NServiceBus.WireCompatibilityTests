284f708b6a218c31191cdbd1f84950338e8f65a4
andreasohlund <andreas.ohlund@particular.net>
2016-12-13 08:48:36 +0100

    Made return initiator async

--- src/Version_6_0/TestRunner.cs
+++ src/Version_6_0/TestRunner.cs
@@ -13,7 +13,7 @@ public static class TestRunner
         await bus.InitiatePubSub().ConfigureAwait(false);
         await bus.InitiateSaga().ConfigureAwait(false);
         await bus.InitiateSendReply().ConfigureAwait(false);
-        bus.InitiateSendReturn();
+        await bus.InitiateSendReturn().ConfigureAwait(false);
 
         await Task.Delay(TimeSpan.FromMinutes(1)).ConfigureAwait(false);
         await bus.Stop().ConfigureAwait(false);
--- src/Version_6_1/SendReturn/SendReturnInitiator.cs
+++ src/Version_6_1/SendReturn/SendReturnInitiator.cs
@@ -5,19 +5,16 @@ using NServiceBus;
 public static class SendReturnInitiator
 {
 
-    public static void InitiateSendReturn(this IEndpointInstance bus)
+    public static async Task InitiateSendReturn(this IEndpointInstance bus)
     {
         foreach (var endpoint in EndpointNames.All)
         {
-            Task.Run(() =>
-            {
-                var sendOptions = new SendOptions();
-                sendOptions.SetDestination(endpoint);
+            var sendOptions = new SendOptions();
+            sendOptions.SetDestination(endpoint);
 
-                var result = bus.Request<int>(new SendReturnMessage(), sendOptions).Result;
-                Asserter.IsTrue(5 == result, "Incorrect property value");
-                SendReturnVerifier.ReplyReceivedFrom.Add(endpoint);
-            });
+            var result = await bus.Request<int>(new SendReturnMessage(), sendOptions).ConfigureAwait(false);
+            Asserter.IsTrue(5 == result, "Incorrect property value");
+            SendReturnVerifier.ReplyReceivedFrom.Add(endpoint);
         }
     }
 }
