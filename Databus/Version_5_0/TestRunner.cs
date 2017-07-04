7ce3d89bef8b33cecdf6b8e4c4f54e4a3a91c8f1
Simon Cropp <simon.cropp@gmail.com>
2017-07-04 15:50:56 +1000

    fix test runner

---
+++ Databus/Version_5_0/TestRunner.cs
@@ -0,0 +1,26 @@
+using System;
+using System.Threading;
+using NServiceBus;
+
+public static class TestRunner
+{
+
+    public static void RunTests(IBus bus)
+    {
+        Thread.Sleep(TimeSpan.FromSeconds(25));
+        bus.InitiateDataBus();
+
+        for (var i = 0; i < 10; i++)
+        {
+            Thread.Sleep(TimeSpan.FromSeconds(10));
+            if (DataBusVerifier.IsFinished())
+            {
+                break;
+            }
+        }
+
+        var disposable = bus as IDisposable;
+        disposable?.Dispose();
+        DataBusVerifier.AssertExpectations();
+    }
+}
